4ca79c2dcc8b6e8b9723502a5bc2a02ffedec37a
Kamalpreet Singh <kamalpreet192001@gmail.com>
2025-05-08 12:54:29 -0400

    api's -2

--- backend/questions/urls.py
+++ backend/questions/urls.py
@@ -5,19 +5,17 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Question URLs
+# Question URLs
     path('questions/', views.QuestionAPIview.as_view(), name='question_list_create'),
-    path('questions/<int:pk>/', views.QuestionAPIview.as_view(), name='question_detail'),
+    path('questions/<int:question_pk>/', views.QuestionAPIview.as_view(), name='question_detail'),
 
-    # Get all responses for a particular question
+#  Responses URLs
     path('questions/<int:question_pk>/responses/', views.ResponseAPIview.as_view(), name='response_list_create'),
+    path('responses/<int:response_pk>/', views.SingleResponseAPIview.as_view(), name='response_detail'),
 
-    # Get all comments for a particular question or response
-    path('questions/<int:question_pk>/comments/', views.CommentAPIview.as_view(), name='comment_list_create'),
-    path('responses/<int:response_pk>/comments/', views.CommentAPIview.as_view(), name='response_comment_list_create'), # to be done
-
-    # Patch or delete a specific response or comment
-    path('responses/<int:pk>/', views.ResponseAPIview.as_view(), name='response_detail'),
-    path('comments/<int:pk>/', views.CommentAPIview.as_view(), name='comment_detail'),
-
+#comment urls    
+    path('responses/<int:response_pk>/comments/', views.CommentAPIview.as_view(), name='response_comment_list_create'), 
+    path('comments/<int:comment_pk>/', views.SingleCommentAPIview.as_view(), name='comment_detail'),
+    path('comments/<int:comment_pk>/comments', views.ReplieAPIview.as_view(), name='comment_comment_list_create'),
+    
 ]
